a5b9b350fe9469a07874224e1d220130058122d0
Pratik Pun <pratikpun@yahoo.com>
2023-07-09 21:57:44 +0100

    modified ProductContext to CategoriesContext, shop only displays the first 4 items of each category

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,14 +1,15 @@
-import { ProductsContext } from "../../contexts/products.context";
 import { useContext } from "react";
-import ProductCard from "../../components/product-card/product-card.component";
+import { CategoriesContext } from "../../contexts/categories.context";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
 import "./shop.styles.scss";
 const Shop = () => {
-  const { products } = useContext(ProductsContext);
+  const { categoriesMap } = useContext(CategoriesContext);
 
   return (
-    <div className="products-container">
-      {products.map((product) => {
-        return <ProductCard key={product.id} product={product} />;
+    <div className="shop-container">
+      {Object.keys(categoriesMap).map((key) => {
+        const products = categoriesMap[key];
+        return <CategoryPreview key={key} title={key} products={products} />;
       })}
     </div>
   );
